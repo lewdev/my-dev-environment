76167b9fa1cfbf91ad98d952244153a41ba45448
Lewis Nakao <lewdev@gmail.com>
2019-04-25 12:56:42 -1000

    Create git-my-repos.bat

---
+++ git-my-repos.bat
@@ -0,0 +1,9 @@
+@echo off
+
+git clone https://github.com/lewdev/my-dev-environment.git
+git clone https://lewdev@bitbucket.org/lewdev/keyanalysis.git
+git clone https://github.com/lewdev/narimono.git
+git clone https://github.com/lewdev/osashizu-builder.git
+git clone https://github.com/lewdev/localnotes.git
+git clone https://github.com/lewdev/regex-text-finder.git
+git clone https://github.com/lewdev/trans.git
